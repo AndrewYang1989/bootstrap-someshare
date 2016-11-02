3a8d5ac553a98c5f4f3bfa3bb63d440be50937d2
AndrewYang1989 <yyj_890626@163.com>
2016-11-02 08:42:04 +0800

    upload header and nav.

--- bootstr/code/js/main.js
+++ bootstr/code/js/main.js
@@ -1,7 +1,7 @@
-/* 
+/*
 * @Author: anchen
 * @Date:   2016-10-31 20:12:38
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-10-31 20:12:44
+* @Last Modified time: 2016-11-01 11:16:51
 */
 
