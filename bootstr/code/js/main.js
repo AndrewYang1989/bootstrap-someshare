e563348af906e6d54dca62d3f1d5fa144a0a5768
andrew.yang <yyj_890627@163.com>
2016-11-03 09:15:46 +0800

    update

--- bootstr/code/js/main.js
+++ bootstr/code/js/main.js
@@ -1,7 +1,23 @@
 /*
-* @Author: anchen
-* @Date:   2016-10-31 20:12:38
-* @Last Modified by:   anchen
-* @Last Modified time: 2016-11-01 11:16:51
-*/
-
+ * @Author: andrew.yang
+ * @Date:   2016-10-31 20:12:38
+ * @Last Modified  :   andrew.yang
+ * @Last Modified time: 2016-11-02 13:41:27
+ */
+$(function() {
+    var resize = function function_name() {
+        // body...
+        var windowWidth = $(window).width();
+        var isSmallScreen = windowWidth < 768;
+        $('#main-ad > .carousel-inner > .item').each(function(i, item) {
+            var $item = $(item);
+            $item.css('backgroundImage', 'url("' + $item.data(isSmallScreen ? 'image-xs' : 'image-lg') + '")');
+            if (isSmallScreen) {
+                $item.html('<img src="'+$item.data('image-xs')+'" alt=""/>')
+            }else{
+                $item.empty();
+            }
+        });
+    }
+    $(window).on('resize', resize).trigger('resize') ;
+})
